b0dfa1a1a44853b9bb5027fae0f59905a4e5bf56
tarequr <tarequrr8@gmail.com>
2024-11-29 19:49:21 +0600

    core user next js link

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,7 +24,13 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav>Home | About</nav>
+        <nav>
+          <ul className="flex gap-6">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+        </nav>
+        <hr />
         {children}
       </body>
     </html>
