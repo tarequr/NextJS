c11b3b441c9ab4e1b30bbf4188d78f8132e3b1f9
tarequr <tarequrr8@gmail.com>
2024-12-01 22:54:57 +0600

    feat static meta data

--- app/about/layout.js
+++ app/about/layout.js
@@ -1,6 +1,11 @@
 import Link from 'next/link'
 import React from 'react'
 
+export const metadata = {
+  title: "About Us",
+  description: "This is a about us project",
+};
+
 function layout({ children }) {
   return (
     <div>
