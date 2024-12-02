f104f748188355ba23799331d034842752bb85ee
tarequr <tarequrr8@gmail.com>
2024-12-02 22:39:52 +0600

    core post data fetched

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,9 +1,20 @@
+import getAllPosts from '@/lib/getAllPosts'
+import Link from 'next/link';
 import React from 'react'
 
-function Post() {
+async function Post() {
+  const posts = await getAllPosts();
   return (
     <div className='mt-6'>
         <h1>All Posts</h1>
+
+        <ul className='mt-6'>
+          { posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
