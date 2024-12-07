2101470a6599b0e6aaaa5f06673e87c7466ffd6f
tarequr <tarequrr8@gmail.com>
2024-12-07 22:46:18 +0600

    core: all comments for single post

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,13 +1,15 @@
 import getPost from '@/lib/getPost';
+import getPostComments from '@/lib/getPostComments';
 import { notFound } from 'next/navigation';
 import React from 'react'
 
 export async function generateMetadata({ params }) {
   const { id } = params;
   const post = await getPost(id);
+  const comments = await getPostComments(id);
 
   if (!post) return notFound();
-  
+
   return {
     title: post.title,
     description: post.body,
